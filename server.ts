0d435642acd67dcd443f7852577a9d05feb0a494
Will Estanislao <72217283+estanislaowillia@users.noreply.github.com>
2022-03-19 21:13:12 -0400

    fixed server.ts

--- server.ts
+++ server.ts
@@ -4,9 +4,9 @@ import fs from 'fs';
 import mime from 'mime-types'; // 3rd-party module
 let lookup = mime.lookup; // Alias for mime.lookup
 
-// const hostname = '127.0.0.1'; // Local host
-// const port = 3000;            // Testing port
-const port = process.env.PORT || 3000; // if environment is not part of server give port 3000 anyway
+const hostname:string = 'localhost'; // Local host
+const port: number = 3000;            // Testing port
+// const port = process.env.PORT || 3000; // if environment is not part of server give port 3000 anyway
 
 // Creating an instance of a server (Immutable)
 // Making a server object
@@ -14,16 +14,19 @@ const port = process.env.PORT || 3000; // if environment is not part of server g
 // res object - what the server responds with to yr request 
 const server = http.createServer((req, res) => 
 {
+    let parsedURL = new URL(req.url as string, "http://"+ hostname + ":" + port);
+    let path = parsedURL.pathname.replace(/^\/+|\/+/g, "");
+    
     console.log(__dirname);
     console.log(req);
 
-    let path = req.url as string;
-    if(path == "/" || path == "/home")
+    if(path == "")
     {
-        path = "/index.html";
+        path = "index.html";
     }
 
-    let mime_type = lookup(path.substring(1)) as string;    // mime-type returns string
+    
+    let file = __dirname + "\\" + path;
 
     console.log(path);
 
@@ -36,6 +39,7 @@ const server = http.createServer((req, res) =>
             return;
         }
         res.setHeader("X-Content-Type-Options", "nosniff"); // Security guard
+        let mime_type = lookup(path) as string;    // mime-type returns string
         res.writeHead(200, {'Content-Type': mime_type});    // fix up matching different mime types to inject stuff
         res.end(data);
     });
@@ -44,5 +48,5 @@ const server = http.createServer((req, res) =>
 // Server starts listening here for user requests ("user req on a port") 
 // Similar to addEventListener
 server.listen(port, () => {
-    console.log(`Sever running at Port ${port}`);
+    console.log(`Sever running at http://${hostname}:${port}/`);
 });
